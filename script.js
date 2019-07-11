07c0bf593a9fad3011ff98756bc3d4cc9ac5a4e6
MuhammadFahmiRidwan <mfahmiridwan50@gmail.com>
2019-07-11 16:16:55 +0700

    final

---
+++ script.js
@@ -0,0 +1,52 @@
+function getPilihanComputer() {
+    const comp = Math.random()
+    if( comp < 0.34 ) return 'batu'
+    if( comp >= 0.34 && comp < 0.67 ) return 'gunting'
+    return 'kertas'
+}
+
+function getHasil(comp, player) {
+    if( player == comp ) return 'SERI!'
+    if( player == 'batu' ) return ( comp == 'gunting' ) ? 'MENANG!' : 'KALAH!'
+    if( player == 'gunting' ) return ( comp == 'batu' ) ? 'KALAH!' : 'MENANG!'
+    if( player == 'kertas' ) return ( comp == 'gunting' ) ? 'KALAH!' : 'MENANG!' 
+}
+
+const pBatu = document.querySelector('.batu')
+pBatu.addEventListener('click', function(){
+    const pilihanComputer = getPilihanComputer()
+    const pilihanPlayer = pBatu.className
+    const hasil = getHasil(pilihanComputer, pilihanPlayer)
+
+    const imgComputer = document.querySelector('.img-computer')
+    imgComputer.setAttribute('src', 'img/' + pilihanComputer + '.png')
+
+    const info = document.querySelector('.info')
+    info.innerHTML = hasil
+})
+
+const pGunting = document.querySelector('.gunting')
+pGunting.addEventListener('click', function(){
+    const pilihanComputer = getPilihanComputer()
+    const pilihanPlayer = pGunting.className
+    const hasil = getHasil(pilihanComputer, pilihanPlayer)
+
+    const imgComputer = document.querySelector('.img-computer')
+    imgComputer.setAttribute('src', 'img/' + pilihanComputer + '.png')
+
+    const info = document.querySelector('.info')
+    info.innerHTML = hasil
+})
+
+const pKertas = document.querySelector('.kertas')
+pKertas.addEventListener('click', function(){
+    const pilihanComputer = getPilihanComputer()
+    const pilihanPlayer = pKertas.className
+    const hasil = getHasil(pilihanComputer, pilihanPlayer)
+
+    const imgComputer = document.querySelector('.img-computer')
+    imgComputer.setAttribute('src', 'img/' + pilihanComputer + '.png')
+
+    const info = document.querySelector('.info')
+    info.innerHTML = hasil
+})
